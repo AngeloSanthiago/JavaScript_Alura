0d14fda06e612a5a5d8aec050a72541f8702bfd3
Angelo <angelonotebook@local.com>
2019-12-10 21:05:20 -0300

    implementação de Testes Personalisados - Iframe

---
+++ TestesPesonalizados/js/TrabalhaIframe.js
@@ -0,0 +1,9 @@
+var iframe = document.querySelector("iframe");
+console.log(iframe);
+
+var iframeBuscaPorId = document.querySelector("#docker");
+console.log(iframeBuscaPorId);
+
+var iframe_h1 = iframe.querySelector("h1"); //nao funciona
+console.log(iframe_h1);
+
