8b42f02dd4820242ee6b43e60f7d84223527157f
Angelo <angelonotebook@local.com>
2019-12-13 19:07:04 -0300

    cap02-video04 - implementação completa - alterando elemento html

--- introducao-javascript-inicial/introducao-javascript/js/principal.js
+++ introducao-javascript-inicial/introducao-javascript/js/principal.js
@@ -2,6 +2,9 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
 var paciente = document.querySelector("#primeiro-paciente");
+// paciente.textContent = "teste de inserção de texto"; // este comando monta um novo elemento na página de forma muito estranha.
+console.log(paciente.querySelector(".info-nome").textContent);
+
 
 var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
@@ -9,7 +12,10 @@ var peso = tdPeso.textContent;
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
-var imc = peso / altura * altura;
+var imc = peso / (altura * altura);
+ 
+var tdImc = paciente.querySelector(".info-imc");
+tdImc.textContent = imc;
 
 console.log(imc);
 
