8ee0ec4be4520faf8b2f6f28cbbf66ce50540a1d
Angelo <angelonotebook@local.com>
2019-12-18 19:59:08 -0300

    Cap04-video01 - implementação completa - Escutando eventos e implementação de funções

--- introducao-javascript-inicial/introducao-javascript/js/principal.js
+++ introducao-javascript-inicial/introducao-javascript/js/principal.js
@@ -20,16 +20,17 @@ for (var i = 0; i < pacientes.length; i++) {
     var alturaEhValido = true;
     
     if (peso <= 0 || peso >= 1000) {
-        console.log("Peso inválido!");
+        // console.log("Peso inválido!");
         pesoEhValido = false;
         tdImc.textContent = "Peso inválido!"
-        // paciente.style.color = "red";
-        // paciente.style.backgroundColor = "lightcoral";
+
+        // paciente.style.color = "red"; // Nao recomendado. Estilo direto no HTML
+        // paciente.style.backgroundColor = "lightcoral"; // Nao recomendado. Estilo direto no HTML
         paciente.classList.add("paciente-invalido");
     }
     
     if (altura <= 0 || altura >= 3.0) {
-        console.log("Altura inválida!");
+        // console.log("Altura inválida!");
         alturaEhValido = false;
         tdImc.textContent = "Altura inválida!"
         paciente.classList.add("paciente-invalido");
@@ -40,3 +41,19 @@ for (var i = 0; i < pacientes.length; i++) {
     }
 }
 
+var botaoAdicionar = document.querySelector("#adicionar-paciente"); 
+
+botaoAdicionar.addEventListener("click", function(){
+    console.log("Oi, cliquei no botão");
+})
+
+
+
+// titulo.addEventListener("click", function(){
+//     console.log("Olha só, estou executando de uma função anonima");
+// });
+
+// titulo.addEventListener("click", mostraMensagem);
+// function mostraMensagem() {
+//     console.log("Olá eu fui clicado");
+// }
