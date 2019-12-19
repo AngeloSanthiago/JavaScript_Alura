58d434b39ab2e07910c3901c9b7874f456da83a3
Angelo <angelonotebook@local.com>
2019-12-19 18:36:54 -0300

    Cap04-Video06 - Implementação até o minuto 6:45

--- introducao-javascript-inicial/introducao-javascript/js/principal.js
+++ introducao-javascript-inicial/introducao-javascript/js/principal.js
@@ -45,7 +45,19 @@ var botaoAdicionar = document.querySelector("#adicionar-paciente");
 
 botaoAdicionar.addEventListener("click", function(event){
     event.preventDefault(); // a propriedade "event" parece ter se tornado do objeto Event apenas por ter se chamado o método preventDefault().
-    console.log("Oi, cliquei no botão");
+    // console.log("Oi, cliquei no botão");
+    var form = document.querySelector("#form-adiciona");
+    //abaixo o comando para acessar o elemento pela propriedade name diretamente
+    // console.log(form.altura.value);
+    // console.log(form.querySelector("#altura")); // forma alternativa a anterior
+    
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+    
 })
 
 
