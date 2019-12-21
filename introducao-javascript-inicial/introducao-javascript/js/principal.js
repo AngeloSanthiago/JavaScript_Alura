31601d7c3cb2970cded11c2210a2270dd3100d77
Angelo <angelonotebook@local.com>
2019-12-21 20:06:08 -0300

    Cap04-Video06 - implementação Completa - Cria elementos e aninha

--- introducao-javascript-inicial/introducao-javascript/js/principal.js
+++ introducao-javascript-inicial/introducao-javascript/js/principal.js
@@ -57,6 +57,28 @@ botaoAdicionar.addEventListener("click", function(event){
     var gordura = form.gordura.value;
 
     var pacienteTr = document.createElement("tr");
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    // imcTd.textContent = imc;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    
+    console.log(pacienteTr);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
     
 })
 
