0c1024b348c8561fbff13b8ee2fb13a9b5d97b45
Angelo <angelonotebook@local.com>
2020-01-08 01:38:26 -0300

    Cap06-video02 - Exibindo erro no preenchimento do formulário

--- introducao-javascript-inicial/introducao-javascript/js/form.js
+++ introducao-javascript-inicial/introducao-javascript/js/form.js
@@ -7,8 +7,10 @@ botaoAdicionar.addEventListener("click", function(event){
     var paciente = obtemPacienteDoFormulario(form);
     var pacienteTr = montaTr(paciente);
     
-    if (!validaPaciente(paciente)) {
-        console.log("Paciente inválido!");
+    var erros = validaPaciente(paciente);
+    if (erros.length > 0) {
+        var mensagemErro = document.querySelector("#mensagem-erro");
+        mensagemErro.textContent = erros;
         return;
     }
 
@@ -49,9 +51,9 @@ function montaTd(dado, classe) {
     return td;
 }
 function validaPaciente(paciente) {
-    if (validaPeso(paciente.peso)) {
-        return true;
-    }else{
-        return false;
-    }
+    var erros = [];
+    if (!validaPeso(paciente.peso)) erros.push("Peso é invalido");
+    
+    if (!validaAltura(paciente.altura)) erros.push("Altura é invalida!");
+    return erros;
 }
