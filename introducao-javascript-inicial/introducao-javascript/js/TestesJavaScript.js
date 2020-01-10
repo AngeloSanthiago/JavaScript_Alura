7b8b6bdbbf38785877e45c27bb7dcabafa070b47
Angelo <angelonotebook@local.com>
2020-01-10 09:48:28 -0300

    Cap06-Exerc05 - Exemplos com a propriedade innerHTML

---
+++ introducao-javascript-inicial/introducao-javascript/js/TestesJavaScript.js
@@ -0,0 +1,16 @@
+teste_innerHTML_umElemento();
+
+function teste_innerHTML_umElemento() {
+    var tableHeadElement = document.querySelector("thead");
+    var trElement = document.querySelector("tr");
+    var tdElement = document.querySelector("td");
+    console.log("Table Head: ");
+    console.log(tableHeadElement);
+
+    console.log("Tr: ");
+    console.log(trElement);
+
+    console.log("Tr innerHTML: " + trElement.innerHTML);
+    console.log("Td innerHTML: " + tdElement.innerHTML);
+
+}
