85540d9a41410b8dcafa6abdcd28d361d0982eef
Angelo <angelonotebook@local.com>
2020-01-12 15:32:33 -0300

    Cap07-Video03 - Event Bubbling - Elemento pai escutando a agindo de acordo com eventoem elementos filhos

--- introducao-javascript-inicial/introducao-javascript/js/remover-paciente.js
+++ introducao-javascript-inicial/introducao-javascript/js/remover-paciente.js
@@ -1,7 +1,16 @@
-var pacientes = document.querySelectorAll(".paciente");
-pacientes.forEach(function(paciente) {
-    paciente.addEventListener("dblclick",function() {
-        // console.log("Fui clicado com um duplo click");
-        this.remove();
-    });
+
+// Atribuindo a ação de remoção ao elemento "table"
+var tabela = document.querySelector("table");
+tabela.addEventListener("dblclick",function(event) {
+    // O "event.taget" neste caso vai ser o "td" e o "parentNode" o "tr"
+    event.target.parentNode.remove();
 });
+
+// Atribuindo a ação de remoção a cada "tr" da tabela
+// var pacientes = document.querySelectorAll(".paciente");
+// pacientes.forEach(function(paciente) {
+//     paciente.addEventListener("dblclick",function() {
+//         // console.log("Fui clicado com um duplo click");
+//         this.remove();  //O comando  "paciente.remove()" funciona igualmente;
+//     });
+// });
