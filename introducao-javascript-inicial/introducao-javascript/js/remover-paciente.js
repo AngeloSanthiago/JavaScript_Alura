7cce26ca94c22c7a75b43311eedb65f760e4c236
Angelo <angelonotebook@local.com>
2020-01-13 00:52:59 -0300

    Cap07-Exerc07 - Filtrando a tag TD para a ação de remoção

--- introducao-javascript-inicial/introducao-javascript/js/remover-paciente.js
+++ introducao-javascript-inicial/introducao-javascript/js/remover-paciente.js
@@ -1,11 +1,13 @@
 // Atribuindo a ação de remoção ao elemento "table"
 var tabela = document.querySelector("table");
 tabela.addEventListener("dblclick",function(event) {
+    if (event.target.tagName == 'TD') {
     // O "event.taget" neste caso vai ser o "td" e o "parentNode" o "tr"
     event.target.parentNode.classList.add("fadeOut");
     setTimeout(function() {
         event.target.parentNode.remove();
     },500);
+    }
 });
 
 // Atribuindo a ação de remoção a cada "tr" da tabela
